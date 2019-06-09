83d98ed077c63e79d29772424fb9a8ded5c69220
Julia Laurindo <ouriqueslaurindo.julia@gmail.com>
2019-06-09 02:15:10 -0300

    comentário

--- src/Objetos.java
+++ src/Objetos.java
@@ -20,7 +20,7 @@ public class Objetos extends Personagens {
 
     @Override
     public void curar() {
-    
+        //oi
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
